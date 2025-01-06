3e16dcaa92cc150f589fbec63c8b83252b34f277
Vladimir Strepkov <MarsAlone@mail.ru>
2025-01-07 07:38:54 +0800

    Добавил нажимную плиту

---
+++ scenes/interactive_objects/pressure_plate.gd
@@ -0,0 +1,20 @@
+extends SwitchObject
+
+# Число объектов стоящих на плите (если 0, то плита неактивна, иначе активна)
+var num_obj_on_plate: int = 0
+
+func _ready() -> void:
+	hint_text = "нажимная плита"
+
+func _on_plate_area_body_entered(_body: Node2D) -> void:
+	print(_body)
+	if num_obj_on_plate == 0:
+		activate()
+		$AnimatedSprite2D.play("on")
+	num_obj_on_plate += 1
+
+func _on_plate_area_body_exited(_body: Node2D) -> void:
+	if num_obj_on_plate == 1:
+		deactivate()
+		$AnimatedSprite2D.play("off")
+	num_obj_on_plate -= 1
